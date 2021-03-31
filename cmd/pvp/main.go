33ec0f18b73724afd5074a0a136d000cce3757d8
Water-W <49912271+Water-W@users.noreply.github.com>
2021-03-31 16:01:14 +0800

    Update main.go

--- cmd/pvp/main.go
+++ cmd/pvp/main.go
@@ -5,7 +5,7 @@ import(
 )
 
 func main() {
-	// 命令行参数给出加入的房间和昵称
+	// 命令行参数给出
 	lisPort := flag.String("l", "", "master listens at 8000")
 	httpPort := flag.String("p", "", "http port at 80001")
 	masterIP := flag.String("m", "", "connects to master with ip:port")
